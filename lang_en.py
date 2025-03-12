3d67e7033610dcc2a62ef109b5be43bd08e525a2
Charles J McDonald <cjmcdonald42@gmail.com>
2025-03-12 11:18:00 -0400

    language proof of concept

---
+++ lang_en.py
@@ -0,0 +1,11 @@
+# English language file
+
+language_menu = """
+    Please choose a language:
+    Choisir une langue:
+
+    1. English
+    2. français
+    """
+choose_language = "Choose a language: (1/2) "
+choose_language_error = "Please choose a valid language."
--- service_counter.py
+++ service_counter.py
@@ -5,3 +5,28 @@
     description: Write an invoice for an automotive service writer.
 """
 
+# Default language is English
+import lang_en as lang
+
+# Choose language
+choosing_a_language = True
+while choosing_a_language:
+    print(lang.language_menu)
+    language_choice = input(lang.choose_language)
+    if language_choice == "1":
+        import lanf_en as lang
+    elif language_choice == "2":
+        import lanf_fr as lang
+    else:
+        print(lang.choose_language_error)
+
+
+# Choose currancy
+
+# Gather new customer information
+
+# Select services
+
+# Calculate total
+
+# Print invoice
