4eb2bf78d74e95790a38ee5afd43b99704e64a9d
Charles J McDonald <cjmcdonald42@gmail.com>
2025-03-12 14:07:44 -0400

    algo complete

---
+++ lang_fr.py
@@ -0,0 +1,12 @@
+# English language file
+
+language_menu = """
+    Please choose a language:
+    Choisir une langue:
+
+    1. English
+    2. le français
+    """
+choose_language = "Choisissez une langue: (1/2) "
+choose_language_error = "Veuillez choisir une langue valide."
+language_choice = "Vous avez choisi le français."
